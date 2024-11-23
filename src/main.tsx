280ab118ddbac54ad452bd6c3ca959d1af0c2a0b
Dmitriy Kudasov <dev.kudasov@gmail.com>
2024-11-23 22:24:54 +0300

    manifast

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
 const manifestUrl =
-  'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+  'https://github.com/devkudasov/twa-hello-world-client/public/tonconnect-manifest.json';
 
 createRoot(document.getElementById('root')!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
